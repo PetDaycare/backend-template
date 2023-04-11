c5e72b6cbd846848c71ef58017239653290c058f
roboter5123 <pascal.roboter@hotmail.com>
2023-04-11 17:57:07 +0200

    Initial Commit

---
+++ src/main/java/com/petdaycare/userservice/SampleRestApplication.java
@@ -0,0 +1,15 @@
+package com.petdaycare.userservice;
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableEncryptableProperties
+public class SampleRestApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SampleRestApplication.class, args);
+    }
+
+}
